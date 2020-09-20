39d2d7d26ae56b32ab0048cb7969942fdd901c37
unknown <thomas.e.cowern@gmail.com>
2020-09-20 05:56:59 -0400

    completed libraryAsset functions

--- Projects/LibraryManagementSystem/LibraryManagementSystem/LibraryServices/LibraryAssetService.cs
+++ Projects/LibraryManagementSystem/LibraryManagementSystem/LibraryServices/LibraryAssetService.cs
@@ -1,56 +1,93 @@
 ﻿using LibraryData;
 using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LibraryServices
 {
     public class LibraryAssetService : ILibraryAsset
     {
         private LibraryContext _context;
+
+        public LibraryAssetService(LibraryContext context)
+        {
+            _context = context;
+        }
         public void Add(LibraryAsset newAsset)
         {
-            throw new NotImplementedException();
+            _context.Add(newAsset);
+            _context.SaveChanges();
         }
 
         public IEnumerable<LibraryAsset> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.LibraryAssets
+                .Include(asset => asset.Status)
+                .Include(asset => asset.Location);
         }
 
         public string GetAuthorOrDirector(int id)
         {
-            throw new NotImplementedException();
+            var isBook = _context.LibraryAssets.OfType<Book>()
+                .Where(asset => asset.Id == id).Any();
+
+            var isVideo = _context.LibraryAssets.OfType<Video>()
+                .Where(asset => asset.Id == id).Any();
+
+            return isBook ?
+                _context.Books.FirstOrDefault(book => book.Id == id).Author :
+                _context.Videos.FirstOrDefault(video => video.Id == id).Director
+                ?? "Unknown";
         }
 
         public LibraryAsset GetById(int id)
         {
-            throw new NotImplementedException();
+            return _context.LibraryAssets
+                .Include(asset => asset.Status)
+                .Include(asset => asset.Location)
+                .FirstOrDefault(asset => asset.Id == id);
+            // GetAll().FirstOrDefault(asset => asset.Id == id);
         }
 
         public LibraryBranch GetCurrentLocation(int id)
         {
-            throw new NotImplementedException();
+            return _context.LibraryAssets.FirstOrDefault(asset => asset.Id == id).Location;
+            // GetById(id).Location;
         }
 
         public string GetDeweyIndex(int id)
         {
-            throw new NotImplementedException();
+            if (_context.Books.Any(book => book.Id == id))
+            {
+                return _context.Books.FirstOrDefault(book => book.Id == id).DeweyIndex;
+            }
+
+            else return "";
         }
 
         public string GetIsbn(int id)
         {
-            throw new NotImplementedException();
+            if (_context.Books.Any(a => a.Id == id))
+            {
+                return _context.Books.FirstOrDefault(a => a.Id == id).ISBN;
+            }
+
+            else return "";
         }
 
         public string GetTitle(int id)
         {
-            throw new NotImplementedException();
+            return _context.LibraryAssets.FirstOrDefault(Book => Book.Id == id).Title;
         }
 
         public string GetType(int id)
         {
-            throw new NotImplementedException();
+            var book = _context.LibraryAssets.OfType<Book>()
+                .Where(b => b.Id == id);
+
+            return book.Any() ? "Book" : "Video";
         }
     }
 }
