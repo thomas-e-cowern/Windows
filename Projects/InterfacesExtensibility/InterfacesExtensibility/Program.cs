dba20997259d11ffc15c532b6f89b68dcfea357d
Thomas Cowern <thomas.e.cowern@gmail.com>
2020-09-22 11:26:11 -0400

    added interface extensibility project

---
+++ Projects/InterfacesExtensibility/InterfacesExtensibility/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace InterfacesExtensibility
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+        public void LogInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
+    }
+    public interface ILogger
+    {
+        void LogError(string message);  // method
+        void LogInfo(string message);  // method
+    }
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.LogInfo($"Migrating started at {DateTime.Now}");
+            _logger.LogInfo($"Migrating ended at {DateTime.Now}");
+        }
+    }
+
+    public class FileLogger : ILogger
+    {
+        private readonly string _path;
+        public FileLogger(string path)
+        {
+            _path = path;
+        }
+        public void LogError(string message)
+        {
+            Log(message, "ERROR");
+        }
+        public void LogInfo(string message)
+        {
+            Log(message, "INFO");
+        }
+        private void Log(string message, string messageType)
+        {
+            using (var streamWriter = new System.IO.StreamWriter(_path, true))
+            {
+                streamWriter.WriteLine(messageType + ": " + message);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new ConsoleLogger());
+            dbMigrator.Migrate();
+
+            Console.WriteLine("\npress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
