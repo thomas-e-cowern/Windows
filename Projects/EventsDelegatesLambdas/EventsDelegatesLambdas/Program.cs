8f2fee5137ad1e46ceb7714d7dbee7ed24792656
Thomas Cowern <thomas.e.cowern@gmail.com>
2020-09-24 11:31:38 -0400

    added events delegates lanbdas project

---
+++ Projects/EventsDelegatesLambdas/EventsDelegatesLambdas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDelegatesLambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int, int> multDel = (m, n, o) => m * n * o;
+            Console.WriteLine(multDel(4, 3, 7));
+            Console.ReadKey();
+        }
+    }
+}
