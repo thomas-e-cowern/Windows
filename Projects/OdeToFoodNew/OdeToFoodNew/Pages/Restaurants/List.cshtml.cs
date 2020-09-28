1fcf3cc4871a76ea3bc114b4a228716c1f78b4a3
Thomas Cowern <thomas.e.cowern@gmail.com>
2020-09-28 10:17:51 -0400

    updated appsettings and List

--- Projects/OdeToFoodNew/OdeToFoodNew/Pages/Restaurants/List.cshtml.cs
+++ Projects/OdeToFoodNew/OdeToFoodNew/Pages/Restaurants/List.cshtml.cs
@@ -5,15 +5,24 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFoodNew.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
-        public string Message { get; set; } 
+        public string Message { get; set; }
+        private readonly IConfiguration config;
+
+        public ListModel(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         public void OnGet()
         {
-            Message = "Hello World"; 
+            // Message = "Hello World";
+            Message = config["Message"];
         }
     }
 }
