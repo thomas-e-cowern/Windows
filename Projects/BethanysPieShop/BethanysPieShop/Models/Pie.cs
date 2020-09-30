fbba555d0c50f24cc30d3cc99aa613ac757b703b
Thomas Cowern <thomas.e.cowern@gmail.com>
2020-09-30 12:00:38 -0400

    added pie shop project

---
+++ Projects/BethanysPieShop/BethanysPieShop/Models/Pie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> AllPies { get; }
+        IEnumerable<Pie> PieOfTheWeek { get; }
+        Pie GetPieById(int pieId);
+    }
+
+    public class Pie
+    {
+        public int PieId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsPieOfTheWeek { get; set; }
+        public bool IsInStock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
