b26d943cd14369607a8889f4a934fc595e8ed2c2
Thomas Cowern <thomas.e.cowern@gmail.com>
2020-10-05 13:47:37 -0400

    updated startup file

--- Projects/TestCrafts/TestCrafts/Startup.cs
+++ Projects/TestCrafts/TestCrafts/Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TestCrafts.Models;
 using TestCrafts.Services;
 
 namespace TestCrafts
@@ -52,6 +55,12 @@ namespace TestCrafts
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapGet("/products", (context) => 
+                {
+                    var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
+                    var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
+                    return context.Response.WriteAsync(json);
+                });
             });
         }
     }
