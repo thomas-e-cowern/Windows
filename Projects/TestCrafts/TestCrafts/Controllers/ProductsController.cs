1b1d187bf3fbdf837dcf1b3c8bf09b2ba98c053d
Thomas Cowern <thomas.e.cowern@gmail.com>
2020-10-05 14:05:07 -0400

    added product controller

---
+++ Projects/TestCrafts/TestCrafts/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestCrafts.Models;
+using TestCrafts.Services;
+
+namespace TestCrafts.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
