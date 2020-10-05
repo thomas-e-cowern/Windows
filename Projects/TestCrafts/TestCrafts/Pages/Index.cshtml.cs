14e1da593c7bb025c6eeeba5878545bcf07b600f
Thomas Cowern <thomas.e.cowern@gmail.com>
2020-10-05 12:05:31 -0400

    added craft view

--- Projects/TestCrafts/TestCrafts/Pages/Index.cshtml.cs
+++ Projects/TestCrafts/TestCrafts/Pages/Index.cshtml.cs
@@ -5,21 +5,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TestCrafts.Models;
+using TestCrafts.Services;
 
 namespace TestCrafts.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
         {
             _logger = logger;
+            ProductService = productService;
         }
 
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
