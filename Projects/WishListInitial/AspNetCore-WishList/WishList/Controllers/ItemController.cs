236427218ffe6c9027801bc89d83bd7fae15b2e7
Thomas Cowern <thomas.e.cowern@gmail.com>
2020-10-08 11:58:59 -0400

    added delete to item controller

---
+++ Projects/WishListInitial/AspNetCore-WishList/WishList/Controllers/ItemController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Items.ToList();
+            return View("Index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            _context.Remove(Id);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
