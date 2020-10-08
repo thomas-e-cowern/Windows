fcf617189cbe2b9c7679663e270b19bc7a44372f
Thomas Cowern <thomas.e.cowern@gmail.com>
2020-10-08 10:37:33 -0400

    added item model

--- Projects/WishListInitial/AspNetCore-WishList/WishList/Data/ApplicationDbContext.cs
+++ Projects/WishListInitial/AspNetCore-WishList/WishList/Data/ApplicationDbContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
+
+        public DbSet<Item> Items { get; set; }
         ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
