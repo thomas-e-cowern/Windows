041885ebcc5b17889f9412cd5c62418c6f411df4
Thomas Cowern <thomas.e.cowern@gmail.com>
2020-10-12 07:18:09 -0400

    added create book page

--- Projects/BookListRazor/BookListRazor/Model/Book.cs
+++ Projects/BookListRazor/BookListRazor/Model/Book.cs
@@ -10,9 +10,12 @@ namespace BookListRazor.Model
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
 
         public string Author { get; set; }
+
+        public string ISBN { get; set; }
     }
 }
