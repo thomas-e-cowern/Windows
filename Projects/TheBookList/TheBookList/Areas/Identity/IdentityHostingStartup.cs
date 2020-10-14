3ab397b6068bd28505457909919c153849a209d8
Thomas Cowern <thomas.e.cowern@gmail.com>
2020-10-14 09:11:59 -0400

    added identity scaffolding

---
+++ Projects/TheBookList/TheBookList/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TheBookList.Data;
+
+[assembly: HostingStartup(typeof(TheBookList.Areas.Identity.IdentityHostingStartup))]
+namespace TheBookList.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
